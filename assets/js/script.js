e117f817629aded4f94a41071021acf0c2862e71
Fa <ngomfa@gmail.com>
2018-10-05 15:39:28 +0200

    exo2

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Les Variables de l'exercice 2.
+var lastName = "Doe";
+var firstName = "John";
+var City = "New-York";
+//Je concaténe  mes chaines de caractéres avec mes variables.
+alert ("Nom : " + lastName + "\n" + "Prénom : " + firstName + "\n" + "Ville : " + City);
